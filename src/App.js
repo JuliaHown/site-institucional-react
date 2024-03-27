2888ee9408e5382d51706359528905b80e5972af
JuliaHown <juliahown87@gmail.com>
2024-03-27 01:45:24 -0300

    iniciando a construção da landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
+import "./App.css";
+import Logo from "./componentes/Logo/Logo";
+import Navigation from "./componentes/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo></Logo>
+        <Navigation></Navigation>
+        <div className="btn-user">
+        <button className="btn-Cadastrar">Cadastrar</button>
+        <button className="btn-Login">Fazer Login</button>
+        </div>
       </header>
     </div>
   );
