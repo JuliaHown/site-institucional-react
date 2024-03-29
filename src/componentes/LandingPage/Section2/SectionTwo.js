a8d711b410ae81b7a019b5c5def10c28b937ab6e
JuliaHown <juliahown87@gmail.com>
2024-03-29 18:28:31 -0300

    pagina principal e sobre nos finalizadas

---
+++ src/componentes/LandingPage/Section2/SectionTwo.js
@@ -0,0 +1,24 @@
+import "./SectionTwo.css";
+
+function SectionTwo() {
+    return (
+        <section className="second-landing-page">
+        <div>
+          <h1 className="texto-destaque">
+            <span>Conveniência</span> e <span>Agilidade</span> <br /> para seu
+            condomínio
+          </h1>
+          <span className="second-texto">
+            A Zé Porteiro simplifica a gestão de encomendas em condomínios,
+            priorizando a comunicação rápida, facilidade na retirada e
+            eliminação da burocracia. Nosso objetivo é facilitar a vida de
+            síndicos, administradores e moradores, oferecendo uma abordagem
+            moderna e eficaz para lidar com o fluxo de encomendas.
+          </span>
+        </div>
+        <button className="btn-contrate">Contrate Agora</button>
+      </section>
+    );
+}
+
+export default SectionTwo
