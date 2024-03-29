761047bccbf4ecda8e151ab8f99d8b308f5f8b7c
JuliaHown <juliahown87@gmail.com>
2024-03-29 18:30:39 -0300

    componentes da landing page adicionados no app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Logo from "./componentes/Logo/Logo";
 import Navigation from "./componentes/Navbar/Navbar";
+import SectionUmLanding from "./componentes/LandingPage/Section1/SectionOne"
+import SectionDois from "./componentes/LandingPage/Section2/SectionTwo"
+
 
 function App() {
   return (
@@ -8,11 +11,16 @@ function App() {
       <header className="App-header">
         <Logo></Logo>
         <Navigation></Navigation>
+
         <div className="btn-user">
-        <button className="btn-Cadastrar">Cadastrar</button>
-        <button className="btn-Login">Fazer Login</button>
+          <button className="btn-Cadastrar">Cadastrar</button>
+          <button className="btn-Login">Fazer Login</button>
         </div>
       </header>
+      
+      <SectionUmLanding></SectionUmLanding>
+      <SectionDois></SectionDois>
+
     </div>
   );
 }
