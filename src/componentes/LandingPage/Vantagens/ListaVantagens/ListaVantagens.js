4ee466e40a0b6950deb3d21a171cd642cf4f8829
JuliaHown <juliahown87@gmail.com>
2024-03-30 00:59:28 -0300

    tela sobre as vantagens finalizada

--- src/componentes/LandingPage/Vantagens/ListaVantagens/ListaVantagens.js
+++ src/componentes/LandingPage/Vantagens/ListaVantagens/ListaVantagens.js
@@ -1,15 +1,21 @@
 import "./ListaVantagens.css";
+import Grow from "../../../../assets/grow.png";
+import Check from "../../../../assets/check.png";
+import Light from "../../../../assets/lightbulb.png";
 
 const listaDasVantagens = [
   "Simplificação da Gestão",
   "Eliminação da Burocracia",
   "Compromisso com a Eficiência e Comodidade",
 ];
+
 const listaDescricao = [
   "Na Zé Porteiro, simplificamos a gestão de encomendas em condomínios para tornar a experiência fácil e eficiente para todos os envolvidos.",
   "Eliminamos a burocracia condominial, agilizando entregas e simplificando procedimentos através de nosso sistema automatizado.",
   "Garantimos eficiência e comodidade em todas as etapas do processo de entrega, com comunicação ágil e opções flexíveis de retirada.",
 ];
+
+const icones = [Light, Grow, Check];
 
 function ListaVantagens() {
   return (
@@ -17,13 +23,18 @@ function ListaVantagens() {
       <ul>
         {listaDasVantagens.map((vantagem, index) => (
           <li key={index}>
+          <div className="icon-wrapper">
+            <img src={icones[index]} alt="Ícone" /> 
+          </div>
+          <div className="content-wrapper">
             <h3>{vantagem}</h3>
             <p>{listaDescricao[index]}</p>
-          </li>
+          </div>
+        </li>
         ))}
       </ul>
     </div>
   );
 }
 
-export default ListaVantagens
+export default ListaVantagens;
