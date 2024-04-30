cfa1aaf12937e8ac0d87a2700013a929ae117263
JuliaHown <juliahown87@gmail.com>
2024-04-30 10:47:52 -0300

    rotas da Home e da página de Login criadas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import GlobalStyle from "./GlobalStyle";
+import Login from "./componentes/PaginaDeLogin/PaginaLogin";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App />
+    <GlobalStyle />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
