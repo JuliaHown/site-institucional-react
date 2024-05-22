f5dffdfd18490101829e66edbccb90fda9ad3bd9
JuliaHown <juliahown87@gmail.com>
2024-05-22 01:58:37 -0300

    melhorias no menu lateral das telas pós login

--- src/componentes/TelasDoCliente/CadastroEncomenda/Componentes/Sidebar.js
+++ src/componentes/TelasDoCliente/CadastroEncomenda/Componentes/Sidebar.js
@@ -4,6 +4,56 @@ import axios from "axios";
 
 import Logo from "../../../../assets/LogoPadrao.svg"
 
+
+function MenuUsuario() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/")
+      .then(response => {
+        setNome(response.data.nome);
+        setEmail(response.data.email);
+      })
+      .catch(error => {
+        console.error("Erro ao buscar usuário:", error);
+      });
+  }, []);
+
+  return (
+    <PageContainer>
+      <Sidebar>
+        <Imagem
+          src={Logo}
+          alt="Logo da empresa Zé Porteiro"
+        />
+        <NomeUsuario>{nome}</NomeUsuario>
+        <EmailUsuario>{email}</EmailUsuario>
+        <Links>
+          <Link onClick={() => window.location.href = '/pagina-inicial'}>
+            Página Inicial
+          </Link>
+          <Link onClick={() => window.location.href = '/cadastrar-encomenda'}>
+            Cadastrar Encomendas
+          </Link>
+          <Link onClick={() => window.location.href = '/estoque'}>
+            Estoque
+          </Link>
+          <Link onClick={() => window.location.href = '/apartamentos'}>
+            Apartamentos
+          </Link>
+          <Link onClick={() => window.location.href = '/porteiros'}>
+            Porteiros
+          </Link>
+          <Link onClick={() => window.location.href = '/login'}>
+            SAIR
+          </Link>
+        </Links>
+      </Sidebar>
+    </PageContainer>
+  );
+}
+
 const PageContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -39,65 +89,16 @@ const Links = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  margin-top: 50px;
+  margin-top: 30px;
   font-weight: 600;
 `;
 
 const Link = styled.div`
   font-family: Poppins, sans-serif;
   margin-top: 10px;
-  font-size: 20px;
+  font-size: 19px;
   line-height: 40px;
   cursor: pointer;
 `;
-
-function MenuUsuario() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    axios.get("sua-url-aqui")
-      .then(response => {
-        setNome(response.data.nome);
-        setEmail(response.data.email);
-      })
-      .catch(error => {
-        console.error("Erro ao buscar usuário:", error);
-      });
-  }, []);
-
-  return (
-    <PageContainer>
-      <Sidebar>
-        <Imagem
-          src={Logo}
-          alt="Logo da empresa Zé Porteiro"
-        />
-        <NomeUsuario>{nome}</NomeUsuario>
-        <EmailUsuario>{email}</EmailUsuario>
-        <Links>
-          <Link onClick={() => window.location.href = '/pagina-inicial'}>
-            Página Inicial
-          </Link>
-          <Link onClick={() => window.location.href = '/cadastrar-encomendas'}>
-            Cadastrar Encomendas
-          </Link>
-          <Link onClick={() => window.location.href = '/estoque'}>
-            Estoque
-          </Link>
-          <Link onClick={() => window.location.href = '/apartamentos'}>
-            Apartamentos
-          </Link>
-          <Link onClick={() => window.location.href = '/porteiros'}>
-            Porteiros
-          </Link>
-          <Link onClick={() => window.location.href = '/login'}>
-            SAIR
-          </Link>
-        </Links>
-      </Sidebar>
-    </PageContainer>
-  );
-}
 
 export default MenuUsuario;
