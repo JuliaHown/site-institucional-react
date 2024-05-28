7ce9e44e6a4f7a0e1ff1f6a805dc4897db658ac5
JuliaHown <juliahown87@gmail.com>
2024-05-27 21:59:14 -0300

    menu responsivo adicionado

--- src/componentes/TelasDoCliente/CadastroEncomenda/Componentes/Sidebar.js
+++ src/componentes/TelasDoCliente/CadastroEncomenda/Componentes/Sidebar.js
@@ -1,35 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import axios from "axios";
-
-import Logo from "../../../../assets/LogoPadrao.svg"
-
+import Logo from "../../../../assets/LogoPadrao.svg";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function MenuUsuario() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/")
-      .then(response => {
-        setNome(response.data.nome);
-        setEmail(response.data.email);
-      })
-      .catch(error => {
-        console.error("Erro ao buscar usuário:", error);
-      });
-  }, []);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <PageContainer>
-      <Sidebar>
-        <Imagem
-          src={Logo}
-          alt="Logo da empresa Zé Porteiro"
-        />
-        <NomeUsuario>{nome}</NomeUsuario>
-        <EmailUsuario>{email}</EmailUsuario>
-        <Links>
+      <HamburgerMenu id="hamburger-menu" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes size={30} color="white" /> : <FaBars size={30} color="white" />}
+      </HamburgerMenu>
+      <Sidebar menuOpen={menuOpen}>
+        <Imagem src={Logo} alt="Logo da empresa Zé Porteiro" />
+        <Links menuOpen={menuOpen}>
           <Link onClick={() => window.location.href = '/pagina-inicial'}>
             Página Inicial
           </Link>
@@ -59,6 +47,7 @@ const PageContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  position: relative;
 `;
 
 const Sidebar = styled.div`
@@ -68,21 +57,39 @@ const Sidebar = styled.div`
   color: #fff;
   padding: 20px;
   width: 200px;
-  margin-left: 6vw;
+  position: fixed;
+  top: 0;
+  left: ${({ menuOpen }) => (menuOpen ? '0' : '-220px')};
+  height: 100vh;
+  background-color: #50623A;
+  transition: left 0.3s ease-in-out;
+  z-index: 1000;
+
+  @media (min-width: 768px) {
+    position: static;
+    width: 200px;
+    margin-left: 6vw;
+    height: auto;
+    background-color: transparent;
+    left: 0;
+    transition: none;
+  }
+
+  /* Aplica a margem superior apenas quando o menu está aberto */
+  ${({ menuOpen }) =>
+    menuOpen &&
+    `
+    & > ${Links} {
+      margin-top: 15vh;
+    }
+    & > ${Imagem} {
+      margin-top: 15vh;
+    }
+  `}
 `;
 
 const Imagem = styled.img`
   width: 160px;
-`;
-
-const NomeUsuario = styled.div`
-  margin-top: 17px;
-  font: 800 28px/109% Montserrat, sans-serif;
-`;
-
-const EmailUsuario = styled.div`
-  margin-top: 4px;
-  font: 13px/169% Open Sans, sans-serif;
 `;
 
 const Links = styled.div`
@@ -99,6 +106,19 @@ const Link = styled.div`
   font-size: 19px;
   line-height: 40px;
   cursor: pointer;
+`;
+
+const HamburgerMenu = styled.div`
+  display: none;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  cursor: pointer;
+  z-index: 1001;
+  
+  @media (max-width: 768px) {
+    display: block;
+  }
 `;
 
 export default MenuUsuario;
